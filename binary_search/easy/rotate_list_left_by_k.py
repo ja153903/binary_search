07c15ec0817b67ebe29c8dc7c75724131726eb93
Jaime Abbariao <jaimeabbariao@gmail.com>
2021-09-18 22:33:33 -0400

    Removing pycache

---
+++ binary_search/easy/rotate_list_left_by_k.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class Solution:
+    def solve(self, nums: List[int], k: int) -> List[int]:
+        lst_len = len(nums)
+        k = k % lst_len
+        
+        if k == 0:
+            return nums
+
+        result = list(nums)
+
+        for i in range(len(nums)):
+            # Rotating to the left means the following
+            # i => (i - k)
+            # this only works in Python
+            result[(i - k) % lst_len] = nums[i]
+
+        return result
+
